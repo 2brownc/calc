a726ef3158e274a04a2daf3d95ad5958f9d432ab
brown <86018533+2brownc@users.noreply.github.com>
2022-06-04 14:53:17 +0530

    formatting

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # calc
 
-* A calculator built using ReactJS.
-* Provides AC and DEL operations.
-* Handles "divide by zero" and other invalid operations.
-* Takes input from keypad in the app and also from the keyboard.
-* Warns when input number is not a safe integer.
+- A calculator built using ReactJS.
+- Provides AC and DEL operations.
+- Handles "divide by zero" and other invalid operations.
+- Takes input from keypad in the app and also from the keyboard.
+- Warns when input number is not a safe integer.
 
 [See the app in action!](https://calc-react.pages.dev/)
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,34 @@
-import react from 'react';
-import AppBar from '@mui/material/AppBar';
-import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton'
-import Box from '@mui/material/Box'
-import GitHubIcon from '@mui/icons-material/GitHub';
-import CalculateIcon from '@mui/icons-material/Calculate';
+import react from "react";
+import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import CalculateIcon from "@mui/icons-material/Calculate";
 
-const GithubLink= ({ link }) => {
+const GithubLink = ({ link }) => {
   return (
     <a href={link} target="_blank">
       <GitHubIcon />
     </a>
   );
-}
+};
 
-const Header = ({heading, gitLink}) => {
+const Header = ({ heading, gitLink }) => {
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <CalculateIcon sx={{
-            display: { xs: "none", md: "flex" },
-            mr: 1
-          }} />
+          <CalculateIcon
+            sx={{
+              display: { xs: "none", md: "flex" },
+              mr: 1,
+            }}
+          />
           <span>{heading}</span>
-          <Box sx={{ flexGrow: 1 }}/>
+          <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="View App Source">
               <IconButton sx={{ p: 0 }}>
@@ -38,6 +40,6 @@ const Header = ({heading, gitLink}) => {
       </Container>
     </AppBar>
   );
-}
+};
 
 export default Header;
